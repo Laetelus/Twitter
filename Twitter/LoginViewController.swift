251bb70b92526594d3d27f4bcc000fcc8e6643a6
Angelo <aalies28@gmail.om>
2022-09-18 17:15:25 -0400

    logout saved in memory

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -17,11 +17,18 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if(UserDefaults.standard.bool(forKey: "userLoggedIn")) == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let Myurl = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: Myurl, success: {
             self.performSegue(withIdentifier: "loginToHome", sender: self)
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
         }, failure: { (Error) in
             print("Unable to log in!")
         })
