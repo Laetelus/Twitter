93a958e6f8d0c71429f20050b9413b4280d503d6
Angelo <aalies28@gmail.om>
2022-09-18 13:03:25 -0400

    first commit

---
+++ Alies_Twitter/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Alies_Twitter
+//
+//  Created by Angelo Alies on 9/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
