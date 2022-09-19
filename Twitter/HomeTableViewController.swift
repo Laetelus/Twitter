3ea1c56561b11060ddf9f377c90afda89bf56fa0
Angelo <aalies28@gmail.om>
2022-09-18 20:37:51 -0400

    TODO: Fix tweet content label

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadTweet()
 
     }
+    
+    func loadTweet(){
+        
+        let Myurl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count":10]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: Myurl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            
+        }, failure: { Error in
+            print("Could not retrieve tweets...`")
+        })
+    }
+        
+
     
     @IBAction func onLogOut(_ sender: Any) {
         TwitterAPICaller.client?.logout()
@@ -24,8 +48,18 @@ class HomeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
         
-        cell.userNameLabel.text = "Some name"
-        cell.tweetContent.text = "Something else"
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        let imageURL = URL(string : (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageURL!)
+        
+        if let imageData = data{
+            cell.profileimageView.image = UIImage(data: imageData)
+        }
+        
         return cell
     }
     
@@ -38,7 +72,7 @@ class HomeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return tweetArray.count
     }
     
 }
